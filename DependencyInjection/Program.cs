83484f9bfa33324e14ec65dbfa0bcf0a5f152cf7
Shikhar10081 <shikharcbr143@gmail.com>
2024-06-27 22:19:23 +0530

    first commit

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using DependencyInjection;
+
+IAccount iac = new SavingAccount();
+Account ac = new Account(iac);
+ ac.PrintAC_Details();
+///////////////////////////////////////////////////////////////////
+///
+//IAccount iac2= new CurrentAccount();
+//Account ac1 = new Account(iac2);
+//ac1.PrintAC_Details();
+Console.ReadLine();
---
+++ DependencyInjection/SavingAccount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection;
+//{
+//       public class SavingAccount
+//    {
+//        public void SavingDelails()
+//        {
+//            Console.WriteLine("Saving Account ");
+//        }
+//    }
+
+//    public class CurrentAccount
+//    {
+//        public void SavingDelails()
+//        {
+//            Console.WriteLine("Current Account ");
+//        }
+//    }
+
+//     public class Account
+//    {                                                       // here my Account class depend on the Saving account and Current account  so there is  tight coupling 
+//        SavingAccount sv = new SavingAccount();
+//        CurrentAccount cv = new CurrentAccount();
+//          public void PrintAccounts()
+//        {
+//            sv.SavingDelails();
+//            cv.SavingDelails();
+//        }
+//    } 
+
+//////////////////////////////////////// Now Dependency Injection 
+
+
+public interface IAccount
+{
+    public void SavingDelails();
+}
+
+
+public class SavingAccount : IAccount
+{
+    public void SavingDelails()
+    {
+        Console.WriteLine("Saving Account ");
+    }
+}
+
+public class CurrentAccount : IAccount
+{
+    public void SavingDelails()
+    {
+        Console.WriteLine("Current Account ");
+    }
+}
+
+public class Account
+{
+    private IAccount accountabc;    // reference variabel 
+    public Account(IAccount obj)   // contructor ke throu ham inject kre ge  referance variable ko;
+    {
+        accountabc = obj;
+    }
+
+    public void PrintAC_Details()
+    {
+        accountabc.SavingDelails();
+
+    }
+}
